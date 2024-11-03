33adce1131b19a69f4e57af020f257a6708b7574
Kosha <igornem1@gmail.com>
2024-11-03 23:39:16 +0200

    workflow

--- repository/CrudRepository.go
+++ repository/CrudRepository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	sq "github.com/Masterminds/squirrel"
 	"github.com/breezeframework/breeze_data/client/db"
 )
 
@@ -10,6 +11,7 @@ type CrudRepository[T any] interface {
 	Create(ctx context.Context, entity T) (int64, error)
 	GetById(ctx context.Context, id int64) (*T, error)
 	GetAll(ctx context.Context) (*[]T, error)
+	GetWhere(ctx context.Context, where sq.Eq) (*[]T, error)
 	Update(ctx context.Context, id int64, entity T) error
 	Delete(ctx context.Context, id int64) error
 }
